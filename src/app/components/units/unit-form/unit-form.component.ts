4d0e91dd27d4ecb889313e3da9fec5bdbb8f7795
sabbir-anwar <protik26@gmail.com>
2019-01-08 15:39:47 +0600

    unit component code modified

--- src/app/components/units/unit-form/unit-form.component.ts
+++ src/app/components/units/unit-form/unit-form.component.ts
@@ -10,58 +10,45 @@ import {getHost} from '../../config';
   
 })
 export class UnitFormComponent implements OnInit {
-  
-   @Input() unit:any;
-   endpoint=getHost();
-   @Output() messageEvent=new EventEmitter<boolean>();
-   newUnitInfo={
-     "name":'',
-     "symbol":'',
-     "description":''
-   }
-
-  constructor(public http:HttpClient) { }
-
-
-  ngOnInit() {
-    
+  @Input() unit:any;
+  endpoint=getHost();
+  @Output() messageEvent=new EventEmitter<boolean>();
+  newUnitInfo={
+    "name":'',
+    "symbol":'',
+    "description":''
   }
 
- submit()
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {  }
+
+  submit()
   {
-    
     if(isUndefined(this.unit))
     {
-      let data=new FormData();
-      data.append("name",this.newUnitInfo.name);
-      data.append("symbol",this.newUnitInfo.symbol);
-      data.append("description",this.newUnitInfo.description);
       let url=this.endpoint+"/api/units/create/"
-      this.http.post(url,data).subscribe((response)=>{
+      this.http.post(url,this.newUnitInfo).subscribe((response)=>{
          console.log(response); 
          this.sendMessageToParent(true);
       })
     }
     else
     {
-      //create a put request
-      let formData=new FormData();
-      formData.append("id",this.unit.id);
-      formData.append("name",this.unit.name);
-      formData.append("symbol",this.unit.symbol);
-      formData.append("description",this.unit.description); 
+      //create a put request 
       console.log(this.unit);
       let endpointUrlForUpdatingUnit=this.endpoint+"/api/units/"+this.unit.id+"/edit/";
-      this.http.put(endpointUrlForUpdatingUnit,formData).subscribe((response)=>{
+      this.http.put(endpointUrlForUpdatingUnit,this.unit).subscribe((response)=>{
           this.sendMessageToParent(true);
       });
-
     }
   }
-
   sendMessageToParent(message:boolean)
   {
     this.messageEvent.emit(message);
   }
-
+  cancel()
+  {
+    this.sendMessageToParent(true); 
+  }
 }
